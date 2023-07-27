f87ca86dc072dc4546d527a915e0c4ed4cab8f53
Sully Birashk <sully.birashk@gmail.com>
2023-07-27 00:03:11 -0700

    Feat: Responsive Hamburger Menu

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 export const Header = () => {
     return (
         <Navbar bg='dark' expand='lg'>
             <Navbar.Brand href="#">Holly Tree Daycare</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarNav" />                
         </Navbar>
     )
 }
