cf45387ec3108b423462cab73c5203f6898056d1
Sully Birashk <sully.birashk@gmail.com>
2023-07-27 00:06:36 -0700

    Feat: Responsive Links on Navbar

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 
 export const Header = () => {
     return (
         <Navbar bg='dark' expand='lg'>
             <Navbar.Brand href="#">Holly Tree Daycare</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarNav" />                
+            <Navbar.Toggle aria-controls="navbarNav" />
+            <Navbar.Collapse id="navbarNav">
+                <Nav className="mr-auto">
+                    <Navbar.Brand href="#">About Us</Navbar.Brand>
+                    <Navbar.Brand href="#">Schedule Tour</Navbar.Brand>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     )
 }
