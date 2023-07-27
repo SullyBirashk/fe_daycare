c052f24e4e41b334706d73d76dedc570c49259ca
Sully Birashk <sully.birashk@gmail.com>
2023-07-27 00:15:08 -0700

    Refactor: Update Syntax

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 export const Header = () => {
     return (
         <Navbar bg='dark' expand='lg'>
-            <Navbar.Brand href="#">Holly Tree Daycare</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarNav" />
-            <Navbar.Collapse id="navbarNav">
-                <Nav className="mr-auto">
-                    <Navbar.Brand href="#">About Us</Navbar.Brand>
-                    <Navbar.Brand href="#">Schedule Tour</Navbar.Brand>
+            <Navbar.Brand href='#'>Holly Tree Daycare</Navbar.Brand>
+            <Navbar.Toggle aria-controls='navbarNav' />
+            <Navbar.Collapse id='navbarNav'>
+                <Nav className='mr-auto'>
+                    <Navbar.Brand href='#'>About Us</Navbar.Brand>
+                    <Navbar.Brand href='#'>Schedule Tour</Navbar.Brand>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
