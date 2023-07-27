e819b0dbcd71214a0bc63dfbad9cd956bc251e64
Sully Birashk <sully.birashk@gmail.com>
2023-07-27 15:56:55 -0700

    Refactor: Move Nav Bar to Left Side when window is resized

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,8 +4,8 @@ import { Navbar, Nav } from 'react-bootstrap';
 export const Header = () => {
     return (
         <Navbar bg='dark' expand='lg'>
-            <Navbar.Brand href='#'>Holly Tree Daycare</Navbar.Brand>
             <Navbar.Toggle aria-controls='navbarNav' />
+            <Navbar.Brand href='#'>Holly Tree Daycare</Navbar.Brand>
             <Navbar.Collapse id='navbarNav'>
                 <Nav className='mr-auto'>
                     <Navbar.Brand href='#'>About Us</Navbar.Brand>
