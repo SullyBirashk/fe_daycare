55745c4b8c1703f5b41195df5b7ddebe35065aa9
Sully Birashk <sully.birashk@gmail.com>
2023-07-26 23:57:51 -0700

    Feat: Brand Name on Nav Bar

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,8 +3,8 @@ import { Navbar, Nav, Button } from 'react-bootstrap';
 
 export const Header = () => {
     return (
-        <Navbar bg='dark'>
-            
+        <Navbar bg='dark' expand='lg'>
+            <Navbar.Brand href="#">Holly Tree Daycare</Navbar.Brand>
         </Navbar>
     )
 }
