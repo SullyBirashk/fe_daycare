6306307afd701b01d60a286c840e8c53328ae8b9
Sully Birashk <sully.birashk@gmail.com>
2023-08-11 13:20:34 -0700

    Refactor: Toggle quotes for HTML and JSX attributes

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,23 +5,23 @@ import './Header.css'
 
 export const Header = () => {
     return (
-        <Navbar bg='light' expand='sm'>
+        <Navbar bg="light" expand="sm">
             <div>
-                <Navbar.Toggle className='mx-2' aria-controls='navbarNav' />
-                <Navbar.Brand className='mx-2' href='#'>
-                    <img className='logo' src={logo} alt="" />
+                <Navbar.Toggle className="mx-2" aria-controls="navbarNav" />
+                <Navbar.Brand className="mx-2" href="#">
+                    <img className="logo" src={logo} />
                 </Navbar.Brand>
             </div>
             
             <Navbar.Offcanvas
-                id={`offcanvasNavbar-expand-sm`}
-                aria-labelledby={`offcanvasNavbarLabel-expand-sm`}
+                id={"offcanvasNavbar-expand-sm"}
+                aria-labelledby={"offcanvasNavbarLabel-expand-sm"}
                 placement="end"
             >
                 <Offcanvas.Header closeButton />
                 <Offcanvas.Body>
                     <Nav className="justify-content-end flex-grow-1 pe-3">
-                    <Navbar.Brand href='#'>About Us</Navbar.Brand>
+                    <Navbar.Brand href="#">About Us</Navbar.Brand>
                     <Navbar.Brand href="#action2">Schedule Tour</Navbar.Brand>
                     <Navbar.Brand href="#action3">Contact Us</Navbar.Brand>
                     </Nav>
