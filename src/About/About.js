73cf1cdd5911f1116e09841643d5d8b70f84dcbb
Sully Birashk <sully.birashk@gmail.com>
2023-08-25 09:56:59 -0700

    Feat: About Us Component

---
+++ src/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header } from '../Header/Header';
+
+export const About = () => {
+    return (
+        <div>
+            <Header/>
+            <h1>About Us</h1>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse ipsam reiciendis repudiandae, pariatur velit perferendis et ea repellat assumenda praesentium ducimus? Maxime, inventore.
+        </div>
+    )}
+            
