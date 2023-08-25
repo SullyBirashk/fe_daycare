b9962424ea7281805c9a2c8609a5ff67e385460c
Sully Birashk <sully.birashk@gmail.com>
2023-08-25 09:57:38 -0700

    Feat: Schedule Tour Component

---
+++ src/Schedule/Schedule.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header } from '../Header/Header';
+
+export const Schedule = () => {
+    return (
+        <div>
+            <Header/>
+            <h1>Schedule Tour</h1>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse ipsam reiciendis repudiandae, pariatur velit perferendis et ea repellat assumenda praesentium ducimus? Maxime, inventore.
+        </div>
+    )}
+            
