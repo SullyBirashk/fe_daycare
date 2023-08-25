b9214c57a9ef5894ccee5da1b54e34cec2e94559
Sully Birashk <sully.birashk@gmail.com>
2023-08-25 09:58:29 -0700

    Refactor

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -7,7 +7,7 @@ export const Header = () =>
         <Navbar bg="light" expand="sm">
             <div>
                 <Navbar.Toggle className="mx-2" aria-controls="navbarNav" />
-                <Navbar.Brand className="mx-2" href="#">
+                <Navbar.Brand className="mx-2" href="/">
                     <img className="logo" src={logo} />
                 </Navbar.Brand>
             </div>
@@ -20,9 +20,9 @@ export const Header = () =>
                 <Offcanvas.Header closeButton />
                 <Offcanvas.Body>
                     <Nav className="justify-content-end flex-grow-1 pe-3">
-                    <Navbar.Brand href="#">About Us</Navbar.Brand>
-                    <Navbar.Brand href="#action2">Schedule Tour</Navbar.Brand>
-                    <Navbar.Brand href="#action3">Contact Us</Navbar.Brand>
+                    <Navbar.Brand href="/about">About Us</Navbar.Brand>
+                    <Navbar.Brand href="/schedule">Schedule Tour</Navbar.Brand>
+                    <Navbar.Brand href="/contact">Contact Us</Navbar.Brand>
                     </Nav>
                 </Offcanvas.Body>
             </Navbar.Offcanvas>
