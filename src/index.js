1ae6e449f04683fd8ee845993207fdf99a8ecfd1
Sully Birashk <sully.birashk@gmail.com>
2023-08-25 09:51:02 -0700

    Feat: React Router About Page Configuration

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import * as ReactDOMClient from 'react-dom/client';
 import { Header } from './Header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createBrowserRouter, RouterProvider, Route, Link} from "react-router-dom";
+import { About } from './About/About';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: (<Header/>),
   },
+  {
+    path: "about",
+    element: (<About/>),
+  }
 ]);
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
